aaa711a28ed003f5c70878f9d28fa9db4bae830e
Petr Karmashev <smonkl@bk.ru>
2016-10-13 14:32:12 +0300

    Game background changed

--- src/js/app.ts
+++ src/js/app.ts
@@ -8,15 +8,13 @@ class SimpleGame {
     }
 
     preload() {
-        this.game.load.image('logo', 'phaser2.png');
     }
 
     create() {
-        var logo = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'logo');
-        logo.anchor.setTo(0.5, 0.5);
+        this.game.stage.backgroundColor = "#020F14";
     }
 }
 
 window.onload = () => {
-    var game = new SimpleGame();
+    new SimpleGame();
 };
