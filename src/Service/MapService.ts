613f7ec36bff68691bafe15db439dd6a567a8bb3
Petr Karmashev <smonkl@bk.ru>
2016-10-17 04:12:35 +0300

    MapService introduced

---
+++ src/Service/MapService.ts
@@ -0,0 +1,19 @@
+import Game = Phaser.Game;
+
+class MapService {
+    game: Game;
+    private starsNumber: number = 30;
+    private brimBounds: number = 500;
+
+    constructor(game: Phaser.Game) {
+        this.game = game;
+    }
+
+    createStar(): void {
+        for (let i = 0; i < this.starsNumber; i++) {
+            let x = this.game.rnd.integerInRange(- this.brimBounds, this.game.world.width + this.brimBounds);
+            let y = this.game.rnd.integerInRange(- this.brimBounds, this.game.world.height + this.brimBounds);
+            this.game.add.sprite(x, y, 'star');
+        }
+    }
+}
